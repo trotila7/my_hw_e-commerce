1a2224ab4142bbc257a19384a810f44d1b49cc62
Lada Anpilogova <lada2fox@gmail.com>
2024-07-22 17:12:20 +0300

    добавлены атрибуты number_of_category и number_of_unique_products в класс Category

--- src/class_category.py
+++ src/class_category.py
@@ -4,7 +4,22 @@ class Category:
     description: str
     goods: list
 
+    number_of_category = 0
+    unique_products = set()
+    number_of_unique_products = 0
+
     def __init__(self, name, description, goods):
         self.name = name
         self.description = description
         self.goods = goods
+
+        Category.number_of_category += 1
+        Category.unique_products.update(self.goods)
+        Category.number_of_unique_products = len(list(Category.unique_products))
+
+
+fruits_1 = Category('fruit', 'Свежие фрукты', ['apple', 'apple', 'orange'])
+
+if __name__ == "__main__":
+    print(Category.number_of_category)
+    print(Category.number_of_unique_products)
