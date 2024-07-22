b45e84b951895a397d09a23d63cbb0c4f774010a
Lada Anpilogova <lada2fox@gmail.com>
2024-07-22 16:58:31 +0300

    Инициализованы классы category и product

--- src/class_category.py
+++ src/class_category.py
@@ -3,3 +3,8 @@ class Category:
     name: str
     description: str
     goods: list
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
--- src/class_product.py
+++ src/class_product.py
@@ -1,1 +1,11 @@
 class Product:
+    name: str
+    description: str
+    price: float
+    quantity_in_stock: int
+
+    def __init__(self, name, description, price,quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
