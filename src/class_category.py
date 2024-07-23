327c3bf4998feef02a865e76720232b84cc0cbfe
Lada Anpilogova <lada2fox@gmail.com>
2024-07-23 18:59:45 +0300

    Для класса Product добавлен метод create_product и описаны геттеры и сеттеры для атрибута цены

--- src/class_category.py
+++ src/class_category.py
@@ -1,3 +1,6 @@
+from src.class_product import Product
+
+
 class Category:
     """ Класс для определения категории товаров"""
     name: str
@@ -22,18 +25,27 @@ class Category:
         list_new_product = new_product.split(" ")
         return self.__goods.extend(list_new_product)
 
+    @property
     def view_product(self):
         """ Метод, который позволяет просмотреть товары в категории """
         list_goods = self.__goods
         return list_goods
 
+    @property
+    def view_prod(self, name):
+        for good in self.__goods:
+            if good == Product.name_prod:
+                print(Product.price_and_quan)
+            return "Что-то не то"
 
-fruits_1 = Category('fruit', 'Свежие фрукты', ['apple', 'apple', 'orange'])
+
+fruits_1 = Category('fruit', 'Свежие фрукты', ['Яблоко', 'Апельсин'])
 new_product = "peach banana"
 
 if __name__ == "__main__":
     # print(Category.number_of_category)
     # print(Category.number_of_unique_products)
-    print(fruits_1.view_product())
-    fruits_1.new_goods(new_product)
-    print(fruits_1.view_product())
+    print(fruits_1.view_product)
+    # fruits_1.new_goods(new_product)
+    # print(fruits_1.view_product)
+    print()
--- src/class_product.py
+++ src/class_product.py
@@ -9,3 +9,34 @@ class Product:
         self.description = description
         self.price = price
         self.quantity_in_stock = quantity_in_stock
+
+    @staticmethod
+    def create_product(name, description, price, quantity_in_stock):
+        return Product(name, description, price, quantity_in_stock)
+
+    @property
+    def name_prod(self):
+        name_prod = self.name
+        return name_prod
+
+    @property
+    def product_info(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity_in_stock} шт."
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self._price = value
+        else:
+            print("Цена введена некорректная. Цена должна быть больше нуля.")
+
+
+apple = Product('Яблоко', 'Зеленое яблоко', 11.4, 150)
+orange = Product("Апельсин", "Сочный апельсин", 14.6, 300)
+
+if __name__ == "__main__":
+    print(apple.price_and_quan)
