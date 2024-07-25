fa7de156888f8d819fb54ea0d99927d73b7b71c8
Lada Anpilogova <lada2fox@gmail.com>
2024-07-25 15:40:21 +0300

    hw 16.2, выделили базовый абстрактный класс, описали общий функционал. Добавили миксин, который выводит в консоль информацию о том, что был создан объект

--- src/class_product.py
+++ src/class_product.py
@@ -1,4 +1,14 @@
-class Product:
+from abc import ABC, abstractmethod
+
+
+class MixinLog:
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        params = [f"{key}='{value}'" for key, value in self.__dict__.items()]
+        return f"Создан объект класса {class_name}, с параметрами: {', '.join(params)}"
+
+
+class Product(ABC, MixinLog):
     name: str
     description: str
     price: float
@@ -11,10 +21,13 @@ class Product:
         self.price = price
         self.quantity_in_stock = quantity_in_stock
         self.color = color
+        print(self.__repr__())
 
+    @abstractmethod
     def __str__(self):
-        return f'{self.name}, {self.price} руб. Остаток: {self.quantity_in_stock} шт.'
+        pass
 
+    @abstractmethod
     def __add__(self, other):
         if isinstance(other, self.__class__):
             total_value_self = self.price * self.quantity_in_stock
@@ -23,11 +36,9 @@ class Product:
         raise TypeError
 
     @classmethod
+    @abstractmethod
     def create_product(cls, *args):
-        """ Метод, который создает товар и возвращает объект, который можно добавлять в список товаров """
-        if cls == Product:
-            return cls(*args)
-        raise NotImplementedError("Создание продукта для этого класса не поддерживается")
+        pass
 
     @property
     def new_price(self):
@@ -57,14 +68,20 @@ class Smartphone(Product):
         self.model = model
         self.internal_memory = internal_memory
 
+    def __str__(self):
+        return (f"{self.name} (Модель: {self.model}, Производительность: {self.performance}), {self.price} руб. "
+                f"Остаток: {self.quantity_in_stock} шт.")
+
+    def __add__(self, other):
+        return super().__add__(other)
+
     @classmethod
-    def create_product(cls, name, description, price, quantity_in_stock, color, performance, model, internal_memory):
+    def create_product(cls, name, description, price, quantity_in_stock, color, performance, model,
+                       internal_memory):
         if cls == Smartphone:
-            """ Метод, который создает товар и возвращает объект, который можно добавлять в список товаров """
-            return cls(name, description, price, quantity_in_stock, color, performance,
-                       model,
-                       internal_memory)
-        return super().create_product(name, description, price, quantity_in_stock, color)
+            """Метод, который создает товар и возвращает объект, который можно добавлять в список товаров"""
+            return cls(name, description, price, quantity_in_stock, color, performance, model, internal_memory)
+        return "Объект не создан"
 
 
 class LawnGrass(Product):
@@ -76,18 +93,22 @@ class LawnGrass(Product):
         self.made_in = made_in
         self.germination = germination
 
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity_in_stock} шт."
+
+    def __add__(self, other):
+        return super().__add__(other)
+
     @classmethod
     def create_product(cls, name, description, price, quantity_in_stock, color, made_in, germination):
         if cls == LawnGrass:
-            """ Метод, который создает товар и возвращает объект, который можно добавлять в список товаров """
+            """Метод, который создает товар и возвращает объект, который можно добавлять в список товаров"""
             return cls(name, description, price, quantity_in_stock, color, made_in, germination)
-        return super().create_product(name, description, price, quantity_in_stock, color)
+        return "Объект не создан"
 
 
 if __name__ == "__main__":
-    apple = Product.create_product('Яблоко', 'Зеленое яблоко', 11.4, 150, 'зеленый')
-    orange = Product.create_product("Апельсин", "Сочный апельсин", 14.6, 300, 'оранжевый')
-    phone = Smartphone.create_product('phone 1', 'Описание', 178999.4, 50, 'black', 12.4, 'new', 128)
-    ordinary_grass = LawnGrass.create_product('Обычная трава', 'Зеленая трава', 500, 1, 'green', 'Russia', 2)
+    phone = Smartphone.create_product("phone 1", "Описание", 178999.4, 50, "black", 12.4, "new", 128)
+    ordinary_grass = LawnGrass.create_product("Обычная трава", "Зеленая трава", 500, 1, "green", "Russia", 2)
     print(phone)
     print(ordinary_grass)
