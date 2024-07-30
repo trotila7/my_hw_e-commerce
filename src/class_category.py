74fdf23fc865e4d7591d6f150fc839f635cd9ef2
Lada Anpilogova <lada2fox@gmail.com>
2024-07-31 01:45:50 +0300

    Реализован метод average_price для Category

--- src/class_category.py
+++ src/class_category.py
@@ -26,7 +26,7 @@ class Category:
 
     def add_product(self, product):
         """ Метод, который позволяет добавить новый товар в список товаров """
-        if isinstance(product, Product):
+        if not isinstance(product, Product):
             raise TypeError("Можно добавить только объекты типа Product.")
         if product.quantity_in_stock <= 0:
             raise ValueError("Товар с нулевым или отрицательным количеством не может быть добавлен.")
@@ -44,11 +44,18 @@ class Category:
         result = [str(product) for product in self.__goods]
         return '\n'.join(result)
 
-    # def goods_info(self):
-    #    result = []
-    #    for product in self.__goods:
-    #        result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity_in_stock} шт.")
-    #    return '\n'.join(result)
+    def average_price(self):
+        """ Метод, который подсчитывает средний ценник всех товаров """
+        try:
+            total_price = sum(good.price * good.quantity_in_stock for good in self.__goods)
+            total_quantity = sum(good.quantity_in_stock for good in self.__goods)
+
+            if total_quantity == 0:
+                raise ZeroDivisionError("В категории нет товаров")
+            avg_price = round(total_price / total_quantity, 2)
+        except ZeroDivisionError:
+            avg_price = 0
+        return f"Средний ценник всех товаров {avg_price} руб."
 
 
 if __name__ == "__main__":
@@ -57,5 +64,4 @@ if __name__ == "__main__":
     all_goods = Category("Товары", "Все товары", [])
     all_goods.add_product(apple)
     all_goods.add_product(orange)
-    print(all_goods.goods_info)
-    print(all_goods)
+    print(Category.average_price(all_goods))
